dca82020aba5c2e068ad592b8cc6889831b81dea
Chaitanya1603 <95305500+Chaitanya1603@users.noreply.github.com>
2024-05-05 17:33:02 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,9 @@ enableCORS = false\n\
 headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Download NLTK data
+python -m nltk.downloader punkt
